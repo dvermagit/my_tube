619604e70dbf7f43925c0fb8705013d817e2082e
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-03-18 09:30:40 +0530

    AI-title

---
+++ src/app/api/videos/workflow/title/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/db";
+import { videos } from "@/db/schema";
+import { serve } from "@upstash/workflow/nextjs";
+import { and, eq } from "drizzle-orm";
+
+interface InputType {
+  userId: string;
+  videoId: string;
+}
+export const { POST } = serve(async (context) => {
+  const input = context.requestPayload as InputType;
+  const { userId, videoId } = input;
+
+  const video = await context.run("get-video", async () => {
+    const [existingVideo] = await db
+      .select()
+      .from(videos)
+      .where(and(eq(videos.userId, userId), eq(videos.id, videoId)));
+
+    if (!existingVideo) {
+      throw new Error("NOT_FOUND");
+    }
+
+    return existingVideo;
+  });
+
+  //use Api here
+  await context.run("update-video", async () => {
+    await db
+      .update(videos)
+      .set({ title: "Updated from background job" })
+      .where(and(eq(videos.id, video.id), eq(videos.userId, video.userId)));
+  });
+
+  await context.run("second-step", () => {
+    console.log("second step ran");
+  });
+});
