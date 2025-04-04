691a1241fd0a824fc6e9298e55432aedc5823089
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-04-04 17:08:26 +0530

    video-reactions

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -3,7 +3,7 @@ import { VideoGetOneOutput } from "../../types";
 import { VideoDescription } from "./video-description";
 import { VideoMenu } from "./video-menu";
 import { VideoOwner } from "./video-owner";
-import { VideoReactions } from "./video-rections";
+import { VideoReactions } from "./video-reactions";
 import { format, formatDistanceToNow } from "date-fns";
 
 interface VideoTopRowProps {
@@ -20,7 +20,7 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
   const expandedViews = useMemo(() => {
     return Intl.NumberFormat("en", {
       notation: "standard",
-    }).format(1000);
+    }).format(video.viewCount);
   }, []);
 
   const compactDate = useMemo(() => {
@@ -36,7 +36,12 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
       <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
         <VideoOwner user={video.user} videoId={video.id} />
         <div className="flex overflow-x-auto sm:min-w-[calc(50%-6px)] sm:justify-end sm:overflow-visible pb-2 -mb-2 sm:pb-0 sm:mb-0 gap-2 ">
-          <VideoReactions />
+          <VideoReactions
+            videoId={video.id}
+            likes={video.likeCount}
+            dislikes={video.dislikeCount}
+            viewerReaction={video.viewerReaction}
+          />
           <VideoMenu videoId={video.id} variant="secondary" />
         </div>
       </div>
