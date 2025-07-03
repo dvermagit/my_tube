d7ba4f51263bcb18e13478d89d51d9041e7bbefb
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-07-04 00:01:17 +0530

    improvements

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -10,7 +10,7 @@ import {
 import { mux } from "@/lib/mux";
 import { db } from "@/db";
 import { videos } from "@/db/schema";
-import { UTApi } from " /server";
+import { UTApi } from "uploadthing/server";
 
 const SIGNING_SECRET = process.env.MUX_WEBHOOK_SECRET;
 
